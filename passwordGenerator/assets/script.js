8d9626d4dffd32b23cde6cec5e7d50d11be40f1d
Ethan M <99540833+Edawgg@users.noreply.github.com>
2022-04-10 23:03:23 -0500

    Update script.js

--- passwordGenerator/assets/script.js
+++ passwordGenerator/assets/script.js
@@ -8,6 +8,21 @@ function writePassword() {
 
   passwordText.value = password;
 
+var password = function() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`!@#$%^&*_=+<>,.?/",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+var i = 0;
+while(i<20)
+{
+	$('#pwcontainer').html( $('#pwcontainer').html()+'<br>'+generatePassword() );
+  i++;
 }
 
 // Add event listener to generate button
